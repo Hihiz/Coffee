81d0470688a1e8b83647f4de0f25bba95aa0c79e
Hihiz <public@mail.ru>
2023-11-14 21:17:55 +0300

    Добавление сущностей

---
+++ Coffee.Domain/Entities/Event.cs
@@ -0,0 +1,11 @@
+﻿namespace Coffee.Domain.Entities
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.UtcNow;
+        public bool IsActive { get; set; }
+    }
+}
