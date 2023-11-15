065a230f51c7ad710619070eb3ea79b4c9f49413
Hihiz <public@mail.ru>
2023-11-15 23:50:29 +0300

    Создание команды на добавление новой категории

---
+++ Coffee.Application/Categories/Commands/CreateCategoryCommand.cs
@@ -0,0 +1,10 @@
+﻿using Coffee.Domain.Entities;
+using MediatR;
+
+namespace Coffee.Application.Categories.Commands
+{
+    public class CreateCategoryCommand : IRequest<Category>
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Coffee.Application/Categories/Commands/CreateCategoryCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Coffee.Application.Interfaces;
+using Coffee.Domain.Entities;
+using MediatR;
+
+namespace Coffee.Application.Categories.Commands
+{
+    public class CreateCategoryCommandHandler : IRequestHandler<CreateCategoryCommand, Category>
+    {
+        private readonly IRepository<Category> _repository;
+
+        public CreateCategoryCommandHandler(IRepository<Category> repository) => (_repository) = (repository);
+
+        public async Task<Category> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+        {
+            Category category = new Category
+            {
+                Name = request.Name
+            };
+
+            try
+            {
+                _repository.Create(category);
+                await _repository.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw new Exception("Ошибка при создании категории");
+            }
+
+            return category;
+        }
+    }
+}
+
