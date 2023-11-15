2f83d5e46d1fb1d8f1eff5391efb9e6fec98e45c
Hihiz <public@mail.ru>
2023-11-15 23:59:35 +0300

    Создание профилей AutoMapper

---
+++ Coffee.Application/Profiles/EventProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Coffee.Application.Events.Commands.CreateEvent;
+using Coffee.Application.Events.Commands.UpdateEvent;
+using Coffee.Domain.Entities;
+
+namespace Coffee.Application.Profiles
+{
+    public class EventProfile : Profile
+    {
+        public EventProfile() 
+        {
+            CreateMap<Event, CreateEventCommand>().ReverseMap();
+            CreateMap<Event, UpdateEventCommand>().ReverseMap();
+        }
+    }
+}
---
+++ Coffee.Application/Profiles/ProductProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Coffee.Application.Products.Commands.CreateProduct;
+using Coffee.Application.Products.Commands.UpdateProduct;
+using Coffee.Application.Products.Queries.GetProductDetail;
+using Coffee.Application.Products.Queries.GetProductList;
+using Coffee.Domain.Entities;
+
+namespace Coffee.Application.Profiles
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductDetailDto>().ReverseMap();
+            CreateMap<Product, ProductListDto>().ReverseMap();
+
+            CreateMap<Product, CreateProductCommand>().ReverseMap();
+            CreateMap<Product, UpdateProductCommand>().ReverseMap();
+        }
+    }
+}
