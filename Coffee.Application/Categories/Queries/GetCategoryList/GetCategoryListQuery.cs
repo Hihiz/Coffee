e9522ce65789854328d0e3a77bdc5f9ca20de1d7
Hihiz <public@mail.ru>
2023-11-15 23:49:20 +0300

    Создание запроса на получение всех категорий

---
+++ Coffee.Application/Categories/Queries/GetCategoryList/GetCategoryListQuery.cs
@@ -0,0 +1,10 @@
+﻿using Coffee.Domain.Entities;
+using MediatR;
+
+namespace Coffee.Application.Categories.Queries.GetCategoryList
+{
+    public class GetCategoryListQuery : IRequest<List<Category>>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Coffee.Application/Categories/Queries/GetCategoryList/GetCategoryListQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using Coffee.Application.Interfaces;
+using Coffee.Domain.Entities;
+using MediatR;
+
+namespace Coffee.Application.Categories.Queries.GetCategoryList
+{
+    public class GetCategoryListQueryHandler : IRequestHandler<GetCategoryListQuery, List<Category>>
+    {
+        private readonly IRepository<Category> _repository;
+
+        public GetCategoryListQueryHandler(IRepository<Category> repository) => (_repository) = (repository);
+
+        public async Task<List<Category>> Handle(GetCategoryListQuery request, CancellationToken cancellationToken)
+        {
+            List<Category> categoryList = await _repository.GetAll();
+
+            return categoryList;
+        }
+    }
+}
