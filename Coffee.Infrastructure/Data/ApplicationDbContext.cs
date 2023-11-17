8244e1417484657985de74728f3f3590facb812e
Hihiz <public@mail.ru>
2023-11-17 23:54:05 +0300

    Имплементация класса контекста данных, создание миграций

---
+++ Coffee.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Coffee.Application.Interfaces;
+using Coffee.Domain.Entities;
+using Coffee.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Coffee.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<long>, long>, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Event> Events { get; set; }
+    }
+}
