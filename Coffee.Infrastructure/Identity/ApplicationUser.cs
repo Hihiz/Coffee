6f8327984a1ee3a3bcdf0c79f557804fb667cc09
Hihiz <public@mail.ru>
2023-11-17 23:54:28 +0300

    Создание класса пользователя

---
+++ Coffee.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Coffee.Infrastructure.Identity
+{
+    public class ApplicationUser : IdentityUser<long>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Дата регистрации
+        /// </summary>
+        public DateTime RegisteredIn { get; set; } = DateTime.UtcNow;
+    }
+}
