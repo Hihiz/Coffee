2eeb001f4b244eccb01db8fe9f37e0ac73060d94
Hihiz <public@mail.ru>
2024-02-07 23:03:03 +0300

    Создать интерфейсы для возвращения статуса

---
+++ Coffee.Application/Interfaces/IBaseStatus.cs
@@ -0,0 +1,15 @@
+﻿namespace Coffee.Application.Interfaces
+{
+    public interface IBaseStatus
+    {
+        int StatusCode { get; set; }
+        string Message { get; set; }
+    }
+
+    public interface IBaseStatus<T>
+    {
+        int StatusCode { get; set; }
+        string Message { get; set; }
+        T Status { get; set; }
+    }
+}
