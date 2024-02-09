2e6d879cc3fefcbe36ad962e9c740b8a1669637e
Hihiz <public@mail.ru>
2024-02-10 02:32:45 +0300

    Создать валидацию для создания продукта

---
+++ Coffee.Application/Products/Commands/CreateProduct/CreateProductCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace Coffee.Application.Products.Commands.CreateProduct
+{
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Название продукта не может быть пустым")
+                .MinimumLength(3).WithMessage("Название продукта слишком короткое");
+
+            RuleFor(x => x.CategoryId)
+                .NotEmpty().WithMessage("Категория продукта не может быть пустым")
+                .GreaterThan(0).WithMessage("Выберите категорию");
+
+        }
+    }
+}
