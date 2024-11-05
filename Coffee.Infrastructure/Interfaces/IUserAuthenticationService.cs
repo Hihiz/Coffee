19319f84f8f9d52f32393b8e17b2f39f1cb607cc
Hihiz <public@mail.ru>
2024-11-06 00:02:07 +0300

    Создать интерфейс аутентификации

---
+++ Coffee.Infrastructure/Interfaces/IUserAuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿using Coffee.Application.Interfaces;
+using Coffee.Infrastructure.Models;
+
+namespace Coffee.Infrastructure.Interfaces
+{
+    public interface IUserAuthenticationService
+    {
+        Task<IBaseStatus> LoginAsync(LoginModel model);
+        Task LogoutAsync();
+        Task<IBaseStatus> RegisterAsync(RegistrationModel model);
+    }
+}
